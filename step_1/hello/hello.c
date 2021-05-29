2711e03b1b0c188c710ccd7afb14a3423c506e1d
icm7216 <icm7216d@gmail.com>
2021-05-30 01:21:52 +0900

    Add code for step_1

---
+++ step_1/hello/hello.c
@@ -0,0 +1,18 @@
+#include "ruby.h"
+
+static VALUE
+hello_say(VALUE self)
+{
+  return rb_str_new_cstr("Hello Ruby");
+}
+
+void
+Init_hello(void)
+{
+  VALUE mHello;  
+  VALUE cHello;
+
+  mHello = rb_define_module("Hello");
+  cHello = rb_define_class_under(mHello, "Hello", rb_cObject);
+  rb_define_method(cHello, "say", hello_say, 0);
+}
